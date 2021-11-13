08447230890c152a89300d25bbee4bcf2f70c13c
paul-michelle <paulmichellegallery@gmail.com>
2021-11-13 13:59:32 +0300

    Add comments functionality

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.core.mail import send_mail
 from django.views.generic import ListView
-from .forms import EmailPostForm
-from .models import Post
+from .forms import EmailPostForm, CommentForm
+from .models import Post, Comment
 
 
 # def post_list(request: wsgi.WSGIRequest) -> HttpResponse:
@@ -31,7 +31,21 @@ def post_detail(request: wsgi.WSGIRequest, year: int, month: int, day: int,
                 post: str) -> HttpResponse:
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year,
                              publish__month=month, publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    comments = post.comments.filter(active=True)
+    comment_form = None
+    new_comment = None
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+
+    if request.method == "GET":
+        comment_form = CommentForm
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments,
+                                                     'new_comment': new_comment, 'comment_form': comment_form})
 
 
 def post_share(request: wsgi.WSGIRequest, post_id: int) -> HttpResponse:
@@ -40,7 +54,7 @@ def post_share(request: wsgi.WSGIRequest, post_id: int) -> HttpResponse:
     post = get_object_or_404(Post, id=post_id, status='published')
 
     if request.method == "POST":
-        rendered_form = EmailPostForm(request.POST)
+        rendered_form = EmailPostForm(data=request.POST)
         if rendered_form.is_valid():
             clean_data = rendered_form.cleaned_data
             who_shares = clean_data["name"]
@@ -52,7 +66,7 @@ def post_share(request: wsgi.WSGIRequest, post_id: int) -> HttpResponse:
             send_mail(subject, message, '', [with_who_shares])
             mail_sent = True
 
-    if request.method != "POST":
+    if request.method == "GET":
         rendered_form = EmailPostForm()
 
     return render(request, 'blog/post/share.html', {'post': post, 'form': rendered_form, 'mail_sent': mail_sent})
