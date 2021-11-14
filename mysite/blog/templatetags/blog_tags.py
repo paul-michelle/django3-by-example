6e2a94f525fb36c474c2f7ded01c136a7e8c62cd
paul-michelle <paulmichellegallery@gmail.com>
2021-11-14 16:25:47 +0300

    Add custom template tags to enrich sidebar

---
+++ mysite/blog/templatetags/blog_tags.py
@@ -0,0 +1,24 @@
+from typing import Dict
+from django import template
+from django.db.models import QuerySet, Count
+from ..models import Post
+
+register = template.Library()
+LATEST_POSTS_COUNT = 3
+POPULAR_POSTS_COUNT = 3
+
+
+@register.simple_tag(name='posts_total')
+def get_published_posts_total() -> int:
+    return Post.published.count()
+
+
+@register.inclusion_tag('blog/post/latest_posts.html')
+def get_latest_posts(count: int = LATEST_POSTS_COUNT) -> Dict[str, QuerySet]:
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
+
+
+@register.simple_tag
+def get_popular_posts(count: int = POPULAR_POSTS_COUNT) -> QuerySet:
+    return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
